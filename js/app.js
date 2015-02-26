f5948cb0fe58610e4b3d86363dd17a3caf3f01bf
Fabio Bier <fab.bier@gmail.com>
2015-02-26 17:19:08 +0100

    Added the ability to configure album art size.

--- js/app.js
+++ js/app.js
@@ -2,10 +2,15 @@
  * jQuery Last.Fm Plugin by Pinceladas da Web
  * Example and documentation at: https://github.com/pinceladasdaweb/Lastfm-Recent-Tracks
  * Copyright (c) 2013
- * Version: 1.0.0 (13-ABR-2013)
+ * Version: 1.1.0 (13-ABR-2013)
  * Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
  * Requires: jQuery v1.9 or later and Handlebars 1.0 or later
 ---------------*/
+
+  //Clean up the URL's
+  function stripslashes( str ) {   
+    return (str+'').replace(/\0/g, '0').replace(/\\([\\'"])/g, '$1');
+  }
 
 var Lastfm = {
 	init: function(config) {
@@ -13,6 +18,11 @@ var Lastfm = {
 		this.template = config.template;
 		this.container = config.container;
 		this.fetch();
+
+	    if(config.artsize == 'small'){imgsize = 0}
+	    if(config.artsize == 'medium'){imgsize = 1}
+	    if(config.artsize == 'large'){imgsize = 2}
+	    if(config.artsize == 'extralarge'){imgsize = 3}
 	},
 	attachTemplate: function() {
 		var template = Handlebars.compile(this.template);
@@ -27,7 +37,7 @@ var Lastfm = {
 
 			self.tracks = $.map(feed, function(track) {
 				return {
-					image: track.image[2]['#text'],
+					image: stripslashes(track.image[imgsize]['#text']),
 					song: track.name,
 					artist: track.artist['#text'],
 					album: track.album['#text'],
@@ -38,11 +48,3 @@ var Lastfm = {
 		});
 	}
 }
-
-Lastfm.init({
-	template: $('#tracks-template').html(),
-	container: $('.container'),
-	username: 'rj',
-	count: 18,
-	apikey: '<API KEY HERE>'
-})
